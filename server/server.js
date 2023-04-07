52cb18e0caf9278976163448dcc7648717b0a1e8
SLeBron <sophieclebron@gmail.com>
2023-04-06 21:52:01 -0600

    added app.listen to server.js

--- server/server.js
+++ server/server.js
@@ -39,3 +39,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
 };
 
 startApolloServer(typeDefs, resolvers);
+
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
