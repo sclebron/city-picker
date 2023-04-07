9b0c9164f38f0ac9d7f8b657af43471b935131df
SLeBron <sophieclebron@gmail.com>
2023-04-06 21:40:48 -0600

    updated server.js

--- server/server.js
+++ server/server.js
@@ -4,3 +4,12 @@ const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
