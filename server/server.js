4b8b3350b5e998080298badc6e1b5825368b6bea
SLeBron <sophieclebron@gmail.com>
2023-04-06 21:45:28 -0600

    updated server.js

--- server/server.js
+++ server/server.js
@@ -13,3 +13,14 @@ const server = new ApolloServer({
     resolvers,
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
